8b0f439f9044bc4e593fbd389221d2ba5549754b
Changlin Song <u7707452@soco-084887.gpu>
2025-10-12 15:20:18 +1100

    train.slurm

--- sc2f/utils/distributed_utils.py
+++ sc2f/utils/distributed_utils.py
@@ -157,7 +157,7 @@ def init_distributed_mode(args):
 
     args.distributed = True
 
-    torch.cuda.set_device(args.gpu)
+    torch.cuda.set_device("cuda:{}".format(args.gpu))
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}'.format(
         args.rank, args.dist_url), flush=True)
